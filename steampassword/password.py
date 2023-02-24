55386c00a96f3c541a4fd7f2af7758883ce67a38
sometastycake <16300181+sometastycake@users.noreply.github.com>
2023-02-25 02:49:39 +0300

    Fix User-Agent

--- steampassword/password.py
+++ steampassword/password.py
@@ -21,7 +21,7 @@ from steampassword.steam import CustomSteam
 class SteamPasswordChange:
 
     BROWSER = (
-        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/'
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/'
         '537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
     )
 
